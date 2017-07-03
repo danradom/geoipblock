4183ceb6fcb51f6252f7fe41a97d710a719ea410
dan radom <danradom@users.noreply.github.com>
2017-07-03 14:19:22 -0600

    Create geolog.sh

---
+++ geolog.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# iptables firewall log analysis
+#
+
+if [ "$EUID" != "0" ]; then
+        echo ""
+        echo "$0 needs to be ran by root.  re-executing with sudo"
+        echo ""
+        exec sudo /bin/bash "$0" "$@"
+fi
+
+
+blocklog="/var/log/iptables.log"
+log="/usr/local/admin/backup/iplog.log"
+
+
+grep geoblock /var/log/iptables.log  |sed -e 's/.*SRC=//' -e 's/\ DST.*DPT=/|/' -e s'/\ .*//' > $log.tmp
+printf "%-32s %-20s %-5s\n" "country" "ip" "port" >> $log
+echo "-------------------------------------------------------------------------" >> $log
+
+while IFS=$'|' read -r -a entry
+do
+        ip=${entry[0]}
+        port=${entry[1]}
+        country=`geoiplookup $ip|sed 's/GeoIP Country Edition://'`
+
+        printf "%-32s %-20s %-5s\n" "$country" "$ip" "$port" >> $log
+done < $log.tmp
+
+
+cat $log
+rm $log
